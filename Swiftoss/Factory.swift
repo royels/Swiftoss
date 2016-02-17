7bef448beb5bcbffa7c104e9ea10f862a505ae7f
royels <docmentz@gmail.com>
2016-02-17 14:00:00 +0100

    changed few things

---
+++ Swiftoss/Factory.swift
@@ -0,0 +1,87 @@
+//
+//  Resource.swift
+//  Swiftoss
+//
+//  Created by Rohan Yelsangikar on 2/14/16.
+//  Copyright © 2016 Royels. All rights reserved.
+//
+
+import Foundation
+
+class Resource {
+    
+    var options = Dictionary<String, String>()
+    
+    init(options: Dictionary<String, String>) {
+        self.options = options
+    }
+    
+    func areas() -> Area {
+        return Area(resource: self)
+    }
+    
+    func addresses() -> Address{
+        return Address(resource: self)
+    }
+    
+    func bank_accounts() -> BankAccount
+    {
+        return BankAccount(resource: self)
+    }
+    
+    func checks() -> Check{
+        return Check(resource: self)
+    }
+    
+    func countries() -> Country {
+        return Country(resource: self)
+    }
+    
+    func jobs() -> Job {
+        return Job(resource: self)
+    }
+    
+    func letters() -> Letter {
+        return Letter(resource: self)
+    }
+    
+    func objects() -> Object{
+        return Object(resource: self)
+    }
+    
+    func postcards() -> Postcard {
+        return Postcard(resource: self)
+
+    }
+    
+    func routes() -> Route {
+        return Route(resource: self)
+    }
+    
+    func settings() -> Setting {
+        return Setting(resource: self)
+    }
+    
+    func states() -> State {
+        return State(resource: self)
+    }
+    
+    func baseUrl() -> String {
+        let toReturn = "\(self.options["protocol"])://\(self.options["apiKey"]):\(self.options["apiURL"])"
+        return toReturn
+
+    }
+    
+    func constructUrl(resourceType: String, resourceID: String = "") -> String {
+        var toReturn = "\(baseUrl())/\(resourceType)/"
+        if resourceID != "" {
+            toReturn += resourceID
+        }
+        return toReturn
+        
+        
+    }
+    
+    
+    
+}
---
+++ Swiftoss/Objects.swift
@@ -0,0 +1,201 @@
+//
+//  Objects.swift
+//  Swiftoss
+//
+//  Created by Rohan Yelsangikar on 2/15/16.
+//  Copyright © 2016 Royels. All rights reserved.
+//
+
+import Foundation
+
+class Address {
+    
+    var resource = Resource(options: [:])
+    init(resource: Resource) {
+       self.resource = resource
+    }
+    
+    
+    
+}
+
+
+
+
+class Area {
+    
+    var resource = Resource(options: [:])
+    init(resource: Resource) {
+        self.resource = resource
+    }
+    
+    
+    
+    
+}
+
+class BankAccount {
+    
+    var resource = Resource(options: [:])
+    init(resource: Resource) {
+        self.resource = resource
+    }
+    
+    
+    
+    
+}
+
+class Job {
+    
+    var resource = Resource(options: [:])
+    init(resource: Resource) {
+        self.resource = resource
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+class Check {
+    
+    var resource = Resource(options: [:])
+    init(resource: Resource) {
+        self.resource = resource
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
+class Letter {
+    
+    var resource = Resource(options: [:])
+    init(resource: Resource) {
+        self.resource = resource
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+class Country {
+    
+    var resource = Resource(options: [:])
+    init(resource: Resource) {
+        self.resource = resource
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+class Object {
+    
+    var resource = Resource(options: [:])
+    init(resource: Resource) {
+        self.resource = resource
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+class Postcard {
+    
+    var resource = Resource(options: [:])
+    init(resource: Resource) {
+        self.resource = resource
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+class Route {
+    
+    var resource = Resource(options: [:])
+    init(resource: Resource) {
+        self.resource = resource
+    }
+    
+    
+    
+    
+    
+}
+
+
+
+
+class State {
+    
+    var resource = Resource(options: [:])
+    init(resource: Resource) {
+        self.resource = resource
+    }
+    
+    
+    
+    
+    
+}
+
+
+class Setting {
+    
+    var resource = Resource(options: [:])
+    init(resource: Resource) {
+        self.resource = resource
+    }
+    
+    
+    
+    
+}
+
